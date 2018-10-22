fe63467620222491d56617dcbd17e0458292a0c0
wiktorlesiak <b00087701@student.itb.ie>
2018-10-22 12:12:52 +0100

    yaes

--- app/src/main/java/com/lesiak/wiktor/online_image/MainActivity.java
+++ app/src/main/java/com/lesiak/wiktor/online_image/MainActivity.java
@@ -3,6 +3,7 @@ package com.lesiak.wiktor.online_image;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,46 +11,73 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.BufferedInputStream;
 import java.io.InputStream;
 import java.net.URL;
+import java.net.URLConnection;
 
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
-    EditText editText;
-    ImageView imageView;
+    Button btn;
+    private EditText et;
+    private ImageView imageView;
     TextView textView3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageView = findViewById(R.id.imageView);
-        editText = findViewById(R.id.editText);
-        button = findViewById(R.id.button);
+
+        btn = (Button)findViewById(R.id.button);
+        et = (EditText)findViewById(R.id.editText);
 
 
-    }
-
-
-    public static Drawable LoadImageFromWebOperations(String url) {
-        try {
-            InputStream is = (InputStream) new URL(url).getContent();
-            Drawable d = Drawable.createFromStream(is, "src name");
-            return d;
-        } catch (Exception e) {
-            return null;
-        }
     }
 
     public void buttonClickMe(View view) {
-    button.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            //url = https://i.imgur.com/f90pbLq.jpg
-            LoadImageFromWebOperations(editText.getText().toString());
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                try {
+                    URL url = new URL(et.getText().toString());
+                    //URL url = new URL("http://farm7.static.flickr.com/6138/5935946400_934994190e_s_d.jpg");
+                    new MyDownloadTask().execute(url);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+class MyDownloadTask extends AsyncTask<URL, Integer, Bitmap> {
+    @Override
+    protected Bitmap doInBackground(URL... params) {
+        URL url = params[0];
+        Bitmap bitmap = null;
+        try {
+            URLConnection connection = url.openConnection();
+            connection.connect();
+            InputStream is = connection.getInputStream();
+            BufferedInputStream bis = new BufferedInputStream(is);
+            bitmap = BitmapFactory.decodeStream(bis);
+            bis.close();
+            //is.close(); THIS IS THE BROKEN LINE
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
-    });
+
+        return bitmap;
     }
 }
+
+    protected void onPostExecute(Bitmap bitmap) {
+        if (bitmap != null) {
+            ImageView myImage = (ImageView)findViewById(R.id.imageView);
+            myImage.setImageBitmap(bitmap);
+        } else {
+            System.out.print("error");
+        }
+    }
+}
+
