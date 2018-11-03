d08751b8e7009d5b075413f7f69798d4d7ce7946
Wiktor Lesiak <B00087701@student.itb.ie>
2018-11-03 20:18:43 +0000

    completed working

--- app/src/main/java/com/lesiak/wiktor/online_image/MainActivity.java
+++ app/src/main/java/com/lesiak/wiktor/online_image/MainActivity.java
@@ -1,21 +1,22 @@
 package com.lesiak.wiktor.online_image;
-
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
     Button button;
     EditText editText;
     ImageView imageView;
@@ -30,26 +31,45 @@ public class MainActivity extends AppCompatActivity {
         button = findViewById(R.id.button);
 
 
+
     }
 
 
-    public static Drawable LoadImageFromWebOperations(String url) {
-        try {
-            InputStream is = (InputStream) new URL(url).getContent();
-            Drawable d = Drawable.createFromStream(is, "src name");
-            return d;
-        } catch (Exception e) {
-            return null;
-        }
-    }
 
     public void buttonClickMe(View view) {
     button.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             //url = https://i.imgur.com/f90pbLq.jpg
-            LoadImageFromWebOperations(editText.getText().toString());
+           // LoadImageFromWebOperations(editText.getText().toString());
+            new DownloadImageTask((ImageView) findViewById(R.id.imageView)).execute(editText.getText().toString());
         }
     });
+    }
+
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
     }
 }
